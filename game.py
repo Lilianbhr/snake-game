6a9caf3979e56e89a65f2c2f2acda085ce9277e9
Lilian <lil.bouhier@gmail.com>
2025-01-03 16:32:34 +0100

    creation de la classe snake et ses principaux attribut ainsi qu'une methode draw + correction de la taille des tuiles

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ pygame.mixer.init()
 
 # Taile des tuilles
 nb_tuiles = 15
-taille_tuiles = 32
+taille_tuiles = 40
 score_hauteur = 32
 
 # Couleures
@@ -24,6 +24,25 @@ screen.fill(couleurs["vert"])
 # Musique
 musique = pygame.mixer.music.load("assets/music/music.mp3")
 pygame.mixer.music.play()
+
+class Snake(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.img_tetes = {
+            "H" : pygame.image.load("assets/images/head_up.png").convert_alpha(),
+            "B": pygame.image.load("assets/images/head_down.png").convert_alpha(),
+            "D": pygame.image.load("assets/images/head_right.png").convert_alpha(),
+            "G": pygame.image.load("assets/images/head_left.png").convert_alpha()
+        }
+        self.direction = "D"
+        self.tete = self.img_tetes[self.direction]
+        self.image = self.tete
+        self.rect = self.image.get_rect()
+        self.rect.x = (nb_tuiles/2) * taille_tuiles
+        self.rect.y = (nb_tuiles/2) * taille_tuiles
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
 # Boucle de jeu
 running = True
